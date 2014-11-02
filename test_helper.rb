55ef91881a246a591b88ac98ecff0f59a9e6d776
張家瑋 <secret>
2014-11-02 18:39:33 +0800

    person test

--- test_helper.rb
+++ test_helper.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 require 'mocha'
-Dir.entries(File.join('..', 'lib')).reject { |a| a =='.' || a == '..'}.each do |f|
-  require File.join('..', 'lib', f)
+require 'pry'
+binding.pry
+Dir.entries('lib').reject { |a| a =='.' || a == '..'}.each do |f|
+  require File.join('lib', f)
 end
